c1990e7e50c6a1fd12ed9917594709a9ae528756
Heidi Guevarra <heidiguevarra12@gmail.com>
2022-04-14 11:09:26 +0800

    login-page-output

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,9 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 </head>
 <body>
+    <div class="login-logo-container">
+        <img src="{{asset('images/logo.png')}}" alt="logo" class="login-logo">
+    </div>
     <div class="container">
         <div class="login-title-container">
             <text class="login-title">Welcome Back</text>
@@ -12,8 +15,14 @@
         </div>
         <form>
             @csrf
-            <input type="text" placeholder="Enter your email">
-            <input type="password" placeholder="Password">
+            <div>
+                <img src="{{asset('images/login-mail.png')}}">
+                <input type="text" placeholder="Enter your email">
+            </div>
+            <div>
+                <img src="{{asset('images/login-password.png')}}">
+                <input type="password" placeholder="Password">
+            </div>
             <button type="button" onclick="alert('Hello World!')"><text>Sign in</text></button>
         </form>
     </div>
