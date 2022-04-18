2ecf4164e71c45591594501464a9045bb2900a2d
Heidi Guevarra <heidiguevarra12@gmail.com>
2022-04-18 22:49:35 +0800

    Updated Login Page
    Ok na yung form with icons.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,26 +5,26 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 </head>
 <body>
-    <div class="login-logo-container">
-        <img src="{{asset('images/logo.png')}}" alt="logo" class="login-logo">
-    </div>
-    <div class="container">
-        <div class="login-title-container">
-            <text class="login-title">Welcome Back</text>
-            <text class="login-subtitle">Reserve your seat</text>
+    <div class="login">
+        <div class="login-logo-container">
+            <img src="{{asset('images/logo.png')}}" alt="logo" class="login-logo">
         </div>
-        <form>
-            @csrf
-            <div>
-                <img src="{{asset('images/login-mail.png')}}">
-                <input type="text" placeholder="Enter your email">
+        <div class="container">
+            <div class="login-title-container">
+                <text class="login-title">Welcome</text>
+                <text class="login-subtitle">Reserve your seat</text>
             </div>
-            <div>
-                <img src="{{asset('images/login-password.png')}}">
-                <input type="password" placeholder="Password">
-            </div>
-            <button type="button" onclick="alert('Hello World!')"><text>Sign in</text></button>
-        </form>
+            <form>
+                @csrf
+                <div>
+                    <input type="text" placeholder="Email" id="email">
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" id="password">
+                </div>
+                <button type="button" onclick="alert('Hello World!')"><text>Sign in</text></button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
