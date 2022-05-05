bc30337f200796b2cb022397cd32dbefe86f4931
Jerome Almoguera <72122328+jeromealmoguera@users.noreply.github.com>
2022-05-05 13:32:13 +0800

    Updated log in and CSS

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Login</title>
+
+    <!-- ICONS -->
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
+
     <link rel="stylesheet" type="text/css" href="{{asset('css/login.css')}}">
 </head>
+
 <body>
     <div class="login">
         <div class="login-logo-container">
-            <img class="login-logo" src="{{asset('images/logo.png')}}" alt="e-machine logo">
+            <img class="login-logo" src="{{asset('images/E_Blue.svg')}}" alt="e-machine logo">
         </div>
         <div class="container">
             <div class="login-title-container">
@@ -16,10 +24,12 @@
             </div>
             <form>
                 @csrf
-                <div>
+                <div class="inner-addon left-addon">
+                    <i class="fi fi-sr-envelope"></i>
                     <input type="text" placeholder="Email" id="email">
                 </div>
-                <div>
+                <div class="inner-addon left-addon">
+                    <i class="fi fi-sr-lock"></i>
                     <input type="password" placeholder="Password" id="password">
                 </div>
                 <button type="button" onclick="alert('Hello World!')"><text>Sign in</text></button>
@@ -27,4 +37,5 @@
         </div>
     </div>
 </body>
+
 </html>
