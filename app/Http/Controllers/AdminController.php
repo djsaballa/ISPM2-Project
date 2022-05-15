bd05ef88a397227c8ca817b136140aa0ade435aa
Jerome Almoguera <72122328+jeromealmoguera@users.noreply.github.com>
2022-05-15 23:30:32 +0800

    Revert "Revert "CSS update""
    This reverts commit 96e9f346e1da4ef72ca0ac34f6ab98c2763f4d8a.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,23 +6,45 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function home(Request $request) 
+    public function home(Request $request)
     {
         return view('admin.home');
     }
 
-    public function bookings(Request $request) 
+    public function bookings(Request $request)
     {
         return view('admin.list-of-bookings');
     }
 
-    public function employees(Request $request) 
+    public function employees(Request $request)
     {
         return view('admin.list-of-employees');
     }
 
-    public function upcomingEvents(Request $request) 
+    public function upcomingEvents(Request $request)
     {
         return view('admin.list-of-upcoming-events');
+    }
+
+
+    // EMILI CONTROLLER
+    public function addBookings(Request $request)
+    {
+        return view('admin.add-bookings');
+    }
+
+    public function addSchedules(Request $request)
+    {
+        return view('admin.add-schedules');
+    }
+
+    public function editBookings(Request $request)
+    {
+        return view('admin.edit-bookings');
+    }
+
+    public function editSchedules(Request $request)
+    {
+        return view('admin.edit-schedules');
     }
 }
--- resources/views/admin/list-of-bookings.blade.php
+++ resources/views/admin/list-of-bookings.blade.php
@@ -2,48 +2,116 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List of Bookings</title>
+    <title>EMachine | Bookings</title>
     <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 </head>
 
 <body>
-    <sideNav>
-        @include('components.sidenavAdmin')
-    </sideNav>
+    <div>
+        <sideNav>
+            @include('components.sidenavAdmin')
+        </sideNav>
 
-    <h1 class="admin-header">Bookings</h1>
+        <h1 class="bookings-header">BOOKINGS</h1>
 
-    <table class="admin-table" id="bookings">
-        <thead class="admin-table-header">
-            <tr>
-                <th class="profile-image-header"></th>
-                <th>Employee</th>
-                <th>Desk</th>
-                <th>Date</th>
-                <th>Booking Tile</th>
-            <tr>
-        </thead>
-        <tbody class="admin-table-row">
-            <tr>
-                <td class="profile-image"></td>
-                <td>Dave Sadsad</td>
-                <td>Desk 12</td>
-                <td>Monday, March 28, 2022</td>
-                <td>Wala, uupo lang</td>
-            </tr>
-        </tbody>
-    </table>
-
+        <div class="bookings-table-container">
+            <table class="bookings-table" id="bookings">
+                <thead class="bookings-table-header">
+                    <tr>
+                        <th class="profile-image-header"></th>
+                        <th>ID</th>
+                        <th>Employee</th>
+                        <th>Desk</th>
+                        <th>Date</th>
+                        <th>Booking Tile</th>
+                        <th></th>
+                    <tr>
+                </thead>
+                <tbody class="bookings-table-row">
+                    <tr>
+                        <td class="profile-image"></td>
+                        <td>1</td>
+                        <td>Dave Sadsad</td>
+                        <td>Desk 23</td>
+                        <td> May 26, 2022</td>
+                        <td>Wala, uupo lang</td>
+                        <td>
+                            <a href="/admin-edit-bookings">
+                                <button type="button" class="bookings-btn">Edit</button>
+                            </a>
+                            <button type="button" class="bookings-cncl-btn">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="profile-image"></td>
+                        <td>2</td>
+                        <td>Melrose Berocil Cortes</td>
+                        <td>Desk 8</td>
+                        <td> May 22, 2022</td>
+                        <td>Wala, uupo lang</td>
+                        <td>
+                            <a href="/admin-edit-bookings">
+                                <button type="button" class="bookings-btn">Edit</button>
+                            </a>
+                            <button type="button" class="bookings-cncl-btn">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="profile-image"></td>
+                        <td>3</td>
+                        <td>Heidi Guevarra</td>
+                        <td>Desk 22</td>
+                        <td> May 22, 2022</td>
+                        <td>Wala, uupo lang</td>
+                        <td>
+                            <a href="/admin-edit-bookings">
+                                <button type="button" class="bookings-btn">Edit</button>
+                            </a>
+                            <button type="button" class="bookings-cncl-btn">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="profile-image"></td>
+                        <td>4</td>
+                        <td>Daniel Emili Gaspar</td>
+                        <td>Desk 12</td>
+                        <td> May 18, 2022</td>
+                        <td>Wala, uupo lang</td>
+                        <td>
+                            <a href="/admin-edit-bookings">
+                                <button type="button" class="bookings-btn">Edit</button>
+                            </a>
+                            <button type="button" class="bookings-cncl-btn">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="profile-image"></td>
+                        <td>5</td>
+                        <td>Daniel John Saballa</td>
+                        <td>Desk 16</td>
+                        <td> May 28, 2022</td>
+                        <td>Wala, uupo lang</td>
+                        <td>
+                            <a href="/admin-edit-bookings">
+                                <button type="button" class="bookings-btn">Edit</button>
+                            </a>
+                            <button type="button" class="bookings-cncl-btn">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <footer>
+            @include('components.footer')
+        </footer>
+    </div>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
 <script>
     jQuery(document).ready(function() {
         jQuery('#bookings').DataTable();
